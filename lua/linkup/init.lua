a4fa0e17b49e3c8ca97161321a58e4ba210aa2e0
cjumel <clement.jumel@gmail.com>
2025-02-06 13:57:37 +0100

    chore!: remove visual mode module
    I don't know how to make this work with user-commands and this is not
    super important, so let's just remove it altogether.

--- lua/linkup/init.lua
+++ lua/linkup/init.lua
@@ -1,6 +1,5 @@
 local api = require("linkup.api")
 local config = require("linkup.config")
-local visual_mode = require("visual_mode")
 
 local M = {}
 
@@ -17,12 +16,7 @@ function M.standard_search()
     error("Linkup API key not found.")
   end
 
-  local text = ""
-  if visual_mode.is_on() then
-    text = visual_mode.get_text()
-  end
-
-  vim.ui.input({ prompt = "Linkup Standard Search", default = text }, function(input)
+  vim.ui.input({ prompt = "Linkup Standard Search" }, function(input)
     if input ~= nil and input ~= "" then
       api.search(input, "standard", "sourcedAnswer", function(response)
         vim.notify(response.answer, vim.log.levels.INFO, { title = "Linkup" })
@@ -36,12 +30,7 @@ function M.deep_search()
     error("Linkup API key not found.")
   end
 
-  local text = ""
-  if visual_mode.is_on() then
-    text = visual_mode.get_text()
-  end
-
-  vim.ui.input({ prompt = "Linkup Deep Search", default = text }, function(input)
+  vim.ui.input({ prompt = "Linkup Deep Search" }, function(input)
     if input ~= nil and input ~= "" then
       api.search(input, "deep", "sourcedAnswer", function(response)
         vim.notify(response.answer, vim.log.levels.INFO, { title = "Linkup" })
